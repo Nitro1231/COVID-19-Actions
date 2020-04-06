1edb49d73ff586d47a4481244c0f5a1cbfe24ba0
Nitro <nitro0@naver.com>
2020-04-06 19:54:19 +0900

    Check directory

--- test_DataUpdate.py
+++ test_DataUpdate.py
@@ -8,6 +8,14 @@ import matplotlib.patches as mpatches
 from datetime import datetime, timezone, timedelta
 
 def test_DataUpdate():
+    try:
+        if not(os.path.isdir('DailyReports')):
+            os.makedirs(os.path.join('DailyReports'))
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            print("Failed to create directory.")
+            raise
+
     # Get current time in UTC.
     c = ntplib.NTPClient()
     # NTP server ip
@@ -46,7 +54,7 @@ def test_DataUpdate():
     ax = data['Deaths'].plot(kind='bar', color=(231/255, 76/255, 60/255,1.0))
     plt.xlabel('Country_Region')
     plt.ylabel('Number of People')
-    plt.title(f'10 Countries with most cases ({targetTime})')
+    plt.title(f'TOP-10 countries with most confirmed cases ({targetTime})')
     plt.legend()
     plt.savefig(f'DailyReports\\{targetTime}.png')
     #plt.show()
